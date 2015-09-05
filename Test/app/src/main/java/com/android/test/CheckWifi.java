381a9bc7e390c32d112408ddd559a3f7780dc47d
Bo Sun <bsun@Bos-MacBook-Pro.local>
2015-09-05 01:02:25 -0500

    adding feature off javier's code to send CSV file from App

--- Test/app/src/main/java/com/android/test/CheckWifi.java
+++ Test/app/src/main/java/com/android/test/CheckWifi.java
@@ -2,13 +2,23 @@ package com.android.test;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -17,7 +27,9 @@ import java.util.List;
 public class CheckWifi extends Activity {
 
     TextView wifiState;
+    Button sendButton;
     WifiManager wifimg;
+    List<ScanResult> scanResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +40,10 @@ public class CheckWifi extends Activity {
         String results = "\r\n";
 
         //TextView to display all the information
-        wifiState = (TextView)findViewById(R.id.wifi_state);
+        wifiState = (TextView) findViewById(R.id.wifi_state);
+        sendButton = (Button) findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new SendButtonListener());
+        sendButton.setEnabled(false);
 
         wifimg = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 
@@ -44,7 +59,9 @@ public class CheckWifi extends Activity {
         results += "\r\n" + wifiProbe(15) + "\r\n";
 
         wifiState.setText(results);
-
+        if(results != null) {
+            sendButton.setEnabled(true);
+        }
     }
 
     /*
@@ -55,7 +72,7 @@ public class CheckWifi extends Activity {
     public String enableWifi() {
         String result;
 
-        if(wifimg.getWifiState() == WifiManager.WIFI_STATE_ENABLED){
+        if (wifimg.getWifiState() == WifiManager.WIFI_STATE_ENABLED) {
             result = "Wifi is enabled\r\n";
         } else {
             result = "Wifi is NOT enabled, enabling WiFi...\r\n";
@@ -87,12 +104,55 @@ public class CheckWifi extends Activity {
         //minimum signal level to consider a hotspot as valid
         int acceptedLevel = maxLevel - tolerance;
 
+        scanResults = wifiScanList;
+
         //foreach loop to get all the hotspots
-        for(ScanResult sr: wifiScanList) {
-            if(sr.level >= acceptedLevel){   //if(sr.level >= minLevel) {
+        for (ScanResult sr : wifiScanList) {
+            if (sr.level >= acceptedLevel) {   //if(sr.level >= minLevel) {
                 probeResult += "\r\n" + sr.toString() + "\r\n";
             }
         }
         return probeResult;
+    }
+
+    public void exportEmailInCSV(final List<ScanResult> scanResults) throws IOException {
+        File file = null;
+        File root = Environment.getExternalStorageDirectory();
+        if (root.canWrite()) {
+            File dir = new File(root.getAbsolutePath() + "/PersonData");
+            dir.mkdirs();
+            file   =   new File(dir, "Data.csv");
+            CSVWriter writer = null;
+            try {
+                writer = new CSVWriter(new FileWriter(file), ',');
+                for (ScanResult sc : scanResults) {
+                    String[] entries = sc.toString().split(","); // array of your values
+                    writer.writeNext(entries);
+                }
+                writer.close();
+            } catch (IOException e) {
+                //error
+            }
+        }
+        Uri u1 = null;
+        u1 = Uri.fromFile(file);
+
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, "CSV file from AP probe App");
+        sendIntent.putExtra(Intent.EXTRA_STREAM, u1);
+        sendIntent.setType("text/html");
+        startActivity(sendIntent);
+    }
+
+    private class SendButtonListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            try {
+                exportEmailInCSV(scanResults);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
