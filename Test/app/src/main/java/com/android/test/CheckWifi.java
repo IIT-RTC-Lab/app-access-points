ae52eaa370e0c64d10aadc6714f019f6ca7125b5
Bo Sun <bsun@Bos-MacBook-Pro.local>
2015-09-11 00:08:38 -0500

    Added json formatter

--- Test/app/src/main/java/com/android/test/CheckWifi.java
+++ Test/app/src/main/java/com/android/test/CheckWifi.java
@@ -4,21 +4,35 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.opencsv.CSVWriter;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.List;
 
 /**
@@ -28,8 +42,10 @@ public class CheckWifi extends Activity {
 
     TextView wifiState;
     Button sendButton;
+    Button sendURLButton;
     WifiManager wifimg;
     List<ScanResult> scanResults;
+    private String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +60,9 @@ public class CheckWifi extends Activity {
         sendButton = (Button) findViewById(R.id.send_button);
         sendButton.setOnClickListener(new SendButtonListener());
         sendButton.setEnabled(false);
+        sendURLButton = (Button) findViewById(R.id.send_button_url);
+        sendURLButton.setOnClickListener(new SendURLButtonListener());
+        sendURLButton.setEnabled(false);
 
         wifimg = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 
@@ -59,8 +78,9 @@ public class CheckWifi extends Activity {
         results += "\r\n" + wifiProbe(15) + "\r\n";
 
         wifiState.setText(results);
-        if(results != null) {
+        if (results != null) {
             sendButton.setEnabled(true);
+            sendURLButton.setEnabled(true);
         }
     }
 
@@ -121,7 +141,7 @@ public class CheckWifi extends Activity {
         if (root.canWrite()) {
             File dir = new File(root.getAbsolutePath() + "/PersonData");
             dir.mkdirs();
-            file   =   new File(dir, "Data.csv");
+            file = new File(dir, "Data.csv");
             CSVWriter writer = null;
             try {
                 writer = new CSVWriter(new FileWriter(file), ',');
@@ -144,7 +164,31 @@ public class CheckWifi extends Activity {
         startActivity(sendIntent);
     }
 
-    private class SendButtonListener implements View.OnClickListener{
+    private String formatJSON() {
+        try {
+            JSONObject parent = new JSONObject();
+            JSONArray jsonArray = new JSONArray();
+            for (ScanResult sr : scanResults) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("SSID", sr.SSID);
+                jsonObject.put("BSSID", sr.BSSID);
+                jsonObject.put("capabilities", sr.capabilities);
+                jsonObject.put("level", String.valueOf(sr.level));
+                jsonObject.put("frequency", String.valueOf(sr.frequency));
+                jsonObject.put("timestamp", String.valueOf(sr.timestamp));
+                jsonArray.put(jsonObject);
+            }
+
+            parent.put("aps", jsonArray);
+            Log.d("output", parent.toString(2));
+            return parent.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private class SendButtonListener implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
@@ -153,6 +197,55 @@ public class CheckWifi extends Activity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private class SendURLButtonListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            data = formatJSON();
+            PostDataTask task = new PostDataTask();
+//            task.execute(); //TODO enable this after we have the URL
+        }
+    }
+
+
+    private class PostDataTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... parames) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpResponse response = null;
+
+            try {
+                String uri = "http://location.bramsoft.com/datapush.php?"
+                        + "&json=" + URLEncoder.encode(data, "UTF-8");
+                Location location = getLocationInfo();
+                uri += "&lat=" + URLEncoder.encode(String.valueOf(location.getLatitude()), "UTF-8")
+                        + "&long=" + URLEncoder.encode(String.valueOf(location.getLongitude()), "UTF-8");
+                HttpPost httppost = new HttpPost(uri);
+                response = httpClient.execute(httppost);
+
+            } catch (ClientProtocolException e) {
+            } catch (IOException e) {
+            }
+            return Integer.toString(response.getStatusLine().getStatusCode());
+        }
+
+        protected void onPostExecute(String responseCode) {
+            onSaveFinished(responseCode);
+        }
+    }
+
+    private Location getLocationInfo() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    }
+
+    private void onSaveFinished(String code) {
+        if (!code.equals("200")) {
+            Toast.makeText(getApplicationContext(), "error saving data", Toast.LENGTH_LONG).show();
+            sendURLButton.setEnabled(true);
         }
     }
 }
